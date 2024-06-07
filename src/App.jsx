7ef766925e1b58811e5f9a902ac93dcec883664b
David Luan Gomes Lima <david0596@digitalcollegesul.local>
2024-06-07 18:06:28 -0300

    exemploPropsDois  manipulando dados simples atraves da multiplicacao de propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import ExemploProps from "./components/ExemploProps"
+import ExemploPropsDois from "./components/ExemploPropsDois"
 
 
 function App() {
+
+  let article = [
+    {key: 1, title: "Título Um", subtitle: "Subtitle Um"},
+    {key: 2, title: "Título Dois", subtitle: "Subtitle Dois"},
+    {key: 3, title: "Título Três", subtitle: "Subtitle Três"},
+    {key: 4, title: "Título Quatro", subtitle: "Subtitle Quatro"},
+    {key: 5, title: "Título Quinto", subtitle: "Subtitle Quinto"},
+  ]
+
+  let conteudo = article.map(art => <ExemploPropsDois key={art.id} title={art.title} subtitle={art.subtitle}/>)
   
   return (
     <>
-      <ExemploProps 
-      title = "Título Um"
-      />
-      <ExemploProps 
-      title = "Título Dois"
-      />
+      {conteudo}
     </>
   )
 }
---
+++ src/components/ExemploPropsDois.jsx
@@ -0,0 +1,9 @@
+export default function ExemploPropsDois(props) {
+    return(
+        <>
+            <h1>{props.title}</h1>
+            <h3>{props.subtitle}</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore eos velit similique sapiente recusandae tempora doloremque accusamus architecto et consequatur vel deserunt iste quo corrupti delectus corporis aspernatur eius, dicta repudiandae. Repellat incidunt nulla exercitationem repudiandae amet explicabo quam voluptatum.</p>
+        </>
+    )
+}
